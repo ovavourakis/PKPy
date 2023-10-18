da907dc282a5d7687e50283a86b9696e163a76b2
Odysseas Vavourakis <ovavourakis@icloud.com>
2023-10-18 18:30:22 +0100

    first try at a ODE solver implementation

--- model.py
+++ model.py
@@ -1,64 +1,89 @@
 import scipy
 
+from parser import Parser
+
 class Compartment():
-    def __init__(self, compartment_dict): # name, type, volume, initial_amount, rate_in, rate_out):
-        self.name = compartment_dict['name']
-        self.type = compartment_dict['type']
-        self.volume = compartment_dict['volume']
-        self.initial_amount = compartment_dict['initial_amount']
-        self.rate_in = compartment_dict['rate_in']
-        self.rate_out = compartment_dict['rate_out']
+    def __init__(self, dict): # name, type, volume, initial_amount, rate_in, rate_out):
+        self.name = dict['name']
+        self.type = dict['type']
+        self.volume = dict['volume']
+        self.initial_amount = dict['initial_amount']
+        self.rate_in = dict['rate_in']
+        self.rate_out = dict['rate_out']
 
 class Model():
     def __init__(self):
 
-        parser = Parser()
+        parser = Parser("system.json")
         basic_params, compartment_list = parser.construct()
 
         # basic parameters
-        self.subcutaneous = basic_params['subcutaneous']
-        self.dose_values = basic_params['dose']['dose_values']
-        self.dose_times = basic_params['dose']['dose_time']
+        self.subcutaneous = basic_params['subcutaneous']            # boolean
+        self.dose_constant = basic_params['dose'][0]                # amount
+        self.dose_type = basic_params['dose'][1]                    # dosage schedule
 
         # create compartment objects
-        self.compartment_list = [Compartment(compartment_dict) for compartment_dict in compartment_list]
-        if subcutaneous:
-            self.central, self.subcutaneous, *self.other_compartments = compartment_list
+        self.compartment_list = [Compartment(dict) for dict in compartment_list]
+        if self.subcutaneous:
+            self.central = compartment_list[0]
+            self.subcutaneous = compartment_list[-1]
+            self.other_compartments = compartment_list[1:-1]
         else:
-            self.central, *self.other_compartments = compartment_list
+            self.central, *self.other_compartments = compartment_list            # TODO: check if this works
 
-        # compartment terms
-        for C in self.compartment_list:
-            transition = C.rate_in * (self.initiamount/ V_c - q_p1 / V_p1)
-        
+    def dose(t):
+        if self.dose_type == "continuous":
+            return self.dose_constant
+        elif self.dose_type == "once":
+            return self.dose_constant if t==0 else 0
+        else:
+            raise ValueError("some error occured in dose(t)")
+    
+    def ode_system(t, y):
+        if self.subcutaneous:
+            central_amount, subcutaneous_amount = y[0], y[-1]
+            other_amounts = y[1:-1]
+        else:
+            central_amount, *other_amounts = y
 
-
-        equations = None # do stuff with the compartment objects
+        # calculate derivatives for peripheral compartments
+        derivatives = []
+        for amount, C in zip(y0, self.other_compartments):
+            deriv = C.rate_in * (y[0] / self.central.volume - amount / C.volume)
+            derivatives.append(deriv)
+        # calculate derivative for subcutaneous and central compartments
+        if self.subcutaneous:
+            der_central = self.subcutaneous.rate_out * y[-1] - y[0] / self.central.volume * self.central.rate_out - sum(derivatives)
+            der_subcutaneous = dose(t) - self.subcutaneous.rate_out * y[-1]
+            return [der_central] + derivatives + [der_subcutaneous]
+        else:
+            der_central = dose(t) - y[0] / self.central.volume * self.central.rate_out - sum(derivatives)
+            return [der_central] + derivatives
 
     def solve(self):
         """
         passes the equations to scipy.integrate.solve_ivp and
         return solutions (timeseries)
         """
+        # time span to project over (change to user-input TODO)
+        t_span = 1000
+        
+        # define initial conditions
+        if self.subcutaneous:
+            y0 = [self.central.initial_amount, self.subcutaneous.initial_amount]
+            for c in self.other_compartments:
+                y0.append(c.initial_amount)
+        else:
+            y0 = [self.central.initial_amount]
+            for c in self.other_compartments:
+                y0.append(c.initial_amount)
 
+        sol = scipy.integrate.solve_ivp(ode_system, t_span, y0)
 
-    #     sol = scipy.integrate.solve_ivp( fun=lambda t, y: rhs(t, y, *args),
-    #     t_span=[t_eval[0], t_eval[-1]],
-    #     y0=y0, t_eval=t_eval
-    # )
+        return sol
         
     def plot(self):
         """
         plots the solutions returned by self.solve()
         """
-        
-
-
-# def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-#     q_c, q_p1 = y
-
-#     transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-
-#     dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-#     dqp1_dt = transition
-#     return [dqc_dt, dqp1_dt]
+        pass
