9586d75bf86bc18856978e479d4d64e5bdbdb5da
Odysseas Vavourakis <ovavourakis@icloud.com>
2023-10-19 15:27:57 +0100

    pip installable now and changed directory structure to be a package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='PKPy',
       version='0.1',
       description='A package to solve simple pharmacokinetic models.',
-      author=[ "James Broster", "Ronald Cvek", "Hamlet Khachatryan", "Odysseas Vavourakis"]
+      author=[ "James Broster", "Ronald Cvek", "Hamlet Khachatryan", "Odysseas Vavourakis"],
       packages=['PKPy'],
       install_requires = ["numpy", "matplotlib", "scipy"],
       license='MIT')
